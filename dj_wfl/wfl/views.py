5089af22cb17902d801a7d47645aae09b117cb77
EC2 Default User <ec2-user@ip-172-31-1-88.us-west-1.compute.internal>
2022-10-31 23:35:18 +0000

    Added Profession views

--- dj_wfl/wfl/views.py
+++ dj_wfl/wfl/views.py
@@ -1,3 +1,32 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from wfl.models import Profession
+from wfl.serializers import ProfessionSerializer
+
+
+'''
+================
+Profession Views
+================
+'''
+
+
+'''
+Creates a list of Professions
+'''
+class ProfessionList(generics.ListCreateAPIView):
+    queryset = Profession.objects.all()
+    serializer_class = ProfessionSerializer
+    
+    
+'''
+Manages CRUD operations for a Profession
+'''
+class ProfessionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profession.objects.all()
+    serializer_class = ProfessionSerializer

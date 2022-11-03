b2c6ae8544817735ceb6ba9083df157165b9c907
Justin Young <ec2-user@ip-172-31-1-88.us-west-1.compute.internal>
2022-11-03 02:03:39 +0000

    Overrode __str__ method for models

--- dj_wfl/wfl/models.py
+++ dj_wfl/wfl/models.py
@@ -14,10 +14,17 @@ DESC
 
 class CommonData(models.Model):
     name = models.CharField('object name', max_length=256)
+
     
     class Meta:
         abstract = True
 
+        
+    def __str__(self):
+        field_names = [x.name for x in self._meta.fields]
+        return str({x: getattr(self, x) for x in field_names})
+    
+    
 '''
 DESC
    Stores data for media storage across models
@@ -26,6 +33,7 @@ DESC
 class MediaData(models.Model):
     media_url = models.CharField('URL of the media asset', max_length=512, unique=True)
     media_file_data_id = models.IntegerField('ID provided by Battle.net endpoints', null=True)
+
     
     class Meta:
         abstract = True
@@ -46,9 +54,14 @@ DESC
 class Profession(CommonData, MediaData):
     profession_id = models.SmallIntegerField('profession ID', primary_key=True)
     is_primary = models.BooleanField('TRUE if the profession is a primary profession', default=False)
+
     
     class Meta:
         db_table = 'profession'
+
+        
+    def __str__(self):
+        return CommonData.__str__(self)
 
 
 '''
@@ -66,9 +79,15 @@ class ProfessionSkillTier(CommonData):
     min_total_skill_level = models.SmallIntegerField('minimum total skill level (eg. 301 for Burning Crusade)', default=0)
     max_total_skill_level = models.SmallIntegerField('maximum total skill level (eg. 375 for Burning Crusade)', default=0)
     is_legacy_tier = models.BooleanField('TRUE if the tier requires previous tiers to unlock', default=False)
+
     
     class Meta:
         db_table = 'profession_skill_tier'
+        
+    
+    def __str__(self):
+        return CommonData.__str__(self)
+
 
 '''
 ===========
